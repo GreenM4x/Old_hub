46e84c7148d36d5ed63b23a0986ec378800e0c26
GreenM4x <greenm4x@gmail.com>
2023-02-28 15:50:52 +0100

    design theme button

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const themed = document.getElementsByClassName("themed");
+/* const themed = document.getElementsByClassName("themed");
 
 for (let theme of themed) {
   theme.addEventListener("click", () => {
     document.documentElement.classList.toggle("dark");
   });
-}
+} */
 
+function myfunction(e) {
+  const doc = document.documentElement;
+  doc.classList.toggle("dark");
+
+  document.querySelectorAll(".inverted").forEach((result) => {
+    result.classList.toggle("invert");
+  });
+
+  const img = e.currentTarget.querySelector("img");
+  if (doc.classList.contains("dark")) {
+    img.src = "/svg/moon-bold.svg";
+  } else {
+    img.src = "/svg/sun-medium.svg";
+  }
+}
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", myfunction);
 /* import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
 const octokit = new Octokit({ 
