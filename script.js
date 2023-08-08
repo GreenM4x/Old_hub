d223d95de3615e1ca4cb56c8e39518ea39770c2f
GreenM4x <greenmax02@gmail.com>
2023-08-08 20:45:11 +0200

    update

--- script.js
+++ script.js
@@ -16,23 +16,3 @@ function myfunction(e) {
 }
 const btn = document.querySelector(".btn");
 btn.addEventListener("click", myfunction);
-/* import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
-
-const octokit = new Octokit({ 
-  auth: "",
-});
-
-const result = await octokit.request(
-  "GET /repos/GreenM4x/projekt_gamedesign/languages",
-  {
-    owner: "GreenM4x",
-    repo: "projekt_gamedesign",
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-      "Content-Type": "application/json",
-    },
-  }
-);
-
-console.log("result", result);
- */
